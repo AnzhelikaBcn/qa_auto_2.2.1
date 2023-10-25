2f059277c7efa2acd08da8f68e33005b67b24699
ANZHELIKA <angela.pikulina@gmail.com>
2023-10-25 20:02:42 +0200

    HW-2-2-1

--- calculations.js
+++ calculations.js
@@ -1,25 +1,32 @@
+
+// Функция increaseBalance принимает два аргумента и увеличивает значение x на значение y, возвращая результат.
 function increaseBalance(x, y) {
   x = x + y;
   return x;
 }
 
-function decreaseBalance(x) {
+// Функция decreaseBalance принимает один аргумент x, но внутри функции используется y, который не был объявлен.
+// Исправим это и вернем результат вычитания.
+function decreaseBalance(x, y) {
 x = x - y;
 return x;
 }
 
+// Функция divideBalanceByAccounts принимает два аргумента и делит x на y, возвращая результат.
+// Строка console.log("x"); находится после return и не будет выполнена, так как код ниже return не выполняется.
 function divideBalanceByAccounts(x, y) {
   x = x / y;
   return x;
-  console.log("x");
 }
 
+// Функция getRestAfterDivision принимает два аргумента и возвращает остаток от деления x на y.
 function getRestAfterDivision(x, y) {
   x = x % y;
   return x;
 }
 
+// Далее, выполним вызовы функций с правильными аргументами и выведем результаты.
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
